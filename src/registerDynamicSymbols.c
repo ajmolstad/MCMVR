b525e55e5b2682ffd377482c5e5932917cc85f4d
Aaron J. Molstad <amolstad@fredhutch.org>
2018-09-06 15:59:48 -0700

    Update registerDynamicSymbols.c

--- src/registerDynamicSymbols.c
+++ src/registerDynamicSymbols.c
@@ -4,7 +4,7 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-void R_init_MatrixLDA(DllInfo* info) {
+void R_init_MCMVR(DllInfo* info) {
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
   R_useDynamicSymbols(info, TRUE);
 }
